d58c8398ac6979a5864d5400f92b06d57f803140
GrapeColor <grapecolor@users.noreply.github.com>
2023-01-03 11:00:46 +0900

    Add logging shard id

--- src/common/Logger.ts
+++ src/common/Logger.ts
@@ -1,13 +1,15 @@
 import log4js from 'log4js';
 import { Settings } from './Settings.js';
 
+const logName = `${Settings.applicationName} [${process.env['SHARDS'] ?? 'NO SHARDING'}]`;
+
 log4js.configure({
 	appenders: {
-		[Settings.applicationName]: { type: 'console' },
+		[logName]: { type: 'console' },
 	},
 	categories: {
-		default: { appenders: [Settings.applicationName], level: Settings.loggingLevel },
+		default: { appenders: [logName], level: Settings.loggingLevel },
 	},
 });
 
-export const Logger = log4js.getLogger(Settings.applicationName);
+export const Logger = log4js.getLogger(logName);
