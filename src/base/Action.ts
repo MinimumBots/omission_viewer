1e01903ef4c656e802e8df7b4516dcde4c81136c
GrapeColor <grapecolor@users.noreply.github.com>
2023-01-09 16:14:08 +0900

    Add logging to debug

--- src/base/Action.ts
+++ src/base/Action.ts
@@ -6,18 +6,19 @@ import type { Client, ClientEvents } from 'discord.js';
 export abstract class Action<EventName extends keyof ClientEvents> {
 	protected readonly abstract service: Service;
 
+	protected readonly actionName = this.constructor.name;
+
 	public constructor(protected readonly bot: Client<true>) {
 	}
 
 	public execute(...args: ClientEvents[EventName]): void {
-		const actionName = this.constructor.name;
-
-		Logger.debug(`Start ${actionName}.`);
+		Logger.debug(`Start ${this.actionName}.`);
+		Logger.debug(this.actionName, 'received:', args.join('\n'));
 
 		this.call(...args)
-			.then((response) => Logger.debug(actionName, 'responded:', response))
-			.catch((error) => Logger.error(actionName, 'threw:', error))
-			.finally(() => Logger.debug(`Finish ${actionName}.`));
+			.then((response) => Logger.debug(this.actionName, 'responded:', response))
+			.catch((error) => Logger.error(this.actionName, 'threw:', `${error}\n\ncaused by:\n`, args.join('\n')))
+			.finally(() => Logger.debug(`Finish ${this.actionName}.`));
 	}
 
 	protected abstract call(...args: ClientEvents[EventName]): Promise<object | null>;
